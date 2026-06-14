c90a2d73e2f84509a5555dd329f01579ff924720
Pratik Narule <pratiknarule88@gmail.com>
2026-06-14 18:58:23 +0530

    13 june 2026

---
+++ Python_Programming/DataDemo.py
@@ -0,0 +1,5 @@
+no=11
+
+print("Value of No is : ",11)
+print("ID of no is :",id(no))
+print("Type of no is : ",type(no))
---
+++ Python_Programming/TypeDemo.py
@@ -0,0 +1,21 @@
+#DataTypes in Python
+
+#Number  
+no=11               #Int
+marks=90.78         #float
+value=8+5j          #complex
+
+print(value)
+print(type(no))
+print(type(marks))
+print(type(value))
+
+gender=True
+
+print(gender)
+print(type(gender))
+
+demo=None
+
+print(demo)
+print(type(demo))
