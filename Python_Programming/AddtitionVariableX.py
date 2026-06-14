25111873e004dfe269f6aba44355355bfa4aba58
Pratik Narule <pratiknarule88@gmail.com>
2026-06-14 19:31:08 +0530

    jun 14 2026

--- Python_Programming/AddtitionVariableX.py
+++ Python_Programming/AddtitionVariableX.py
@@ -10,3 +10,4 @@ No2= int(input())
 Ans=No1+No2
 
 print("Addition is :",Ans)
+        
---
+++ Python_Programming/SequenceXX.py
@@ -0,0 +1,10 @@
+
+print("Enter First Number")
+No1=int(input())
+
+print("Enter Second Number")
+No2=int(input())
+
+Ans=No1*No2
+
+print("Mulitpication is :",Ans)
